00883e7cf18d8a501294eeceb76a8449a301ba6b
paul13131313 <hiroshinagano0113@gmail.com>
2026-04-07 21:37:55 +0900

    v3.4 Cesium CESIUM_BASE_URL をwindowグローバルで設定+vite-plugin-cesium除去

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import cesium from 'vite-plugin-cesium'
 
 export default defineConfig({
   base: '/mokutekichi/',
-  plugins: [react(), tailwindcss(), cesium()],
+  plugins: [react(), tailwindcss()],
   define: {
     CESIUM_BASE_URL: JSON.stringify('/mokutekichi/cesium/'),
   },
