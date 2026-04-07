0f739c290aaac52c94b064ec6ab81e90ba80d0d8
paul13131313 <hiroshinagano0113@gmail.com>
2026-04-07 21:41:03 +0900

    v3.5 CESIUM_BASE_URLをHTMLのinline scriptで設定（モジュール読み込み前に確実に設定）

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,4 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// Cesiumの静的アセットのベースURLを設定
-;(window as unknown as Record<string, string>).CESIUM_BASE_URL = '/mokutekichi/cesium/'
-
 createRoot(document.getElementById('root')!).render(<App />)
